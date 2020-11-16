67fe9fb3a509fc60c9ceb549b82465f31577c49b
vagrant <vagrant@homestead>
2020-11-16 10:27:29 +0800

    用户界面 - 商品详情页

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -49,4 +49,14 @@ class ProductsController extends Controller
                 'order' => $order,
             ]]);
     }
+
+    public function show(Request $request, Product $product)
+    {
+        // 判断商品是否已经上架，如果没有上架则抛出异常。
+        if (!$product->on_sale) {
+            throw new \Exception('商品未上架');
+        }
+
+        return view('products.show', ['product' => $product]);
+    }
 }
